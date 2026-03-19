8f16cbaa9fcd9b6ee92533c65156685a4741bd3e
ksmith2002 <ksmith609@gatech.edu>
2026-03-19 16:26:31 -0400

    add baseline model

---
+++ models/baseline.py
@@ -0,0 +1,112 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader
+from sklearn.metrics import classification_report, confusion_matrix
+
+data_dir = "dataset"
+batch_size = 16
+num_classes = 4
+num_epochs = 10
+lr = 1e-4
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# We could (probably should) add randomization here, like flipping/rotation
+# Transformers to preprocess data (change to grayscale, resize, make to tensor, normalize)
+# The normalization is for image net normalization, what it was originally trained on
+train_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=3),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+val_test_transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=3),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+# Load the datasets
+train_dataset = datasets.ImageFolder(os.path.join(data_dir, "train"), transform=train_transform)
+val_dataset = datasets.ImageFolder(os.path.join(data_dir, "val"), transform=val_test_transform)
+test_dataset = datasets.ImageFolder(os.path.join(data_dir, "test"), transform=val_test_transform)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+# Get the resnet model, plus loss and optimizer
+model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+model.fc = nn.Linear(model.fc.in_features, num_classes)
+model = model.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+# Training
+best_val_acc = 0.0
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    for images, labels in train_loader:
+        # Load in images
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        # Results
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        # Update loss/predictions
+        running_loss += loss.item() * images.size(0)
+        preds = torch.argmax(outputs, dim=1)
+        # Check for accuracy
+        correct += (preds == labels).sum().item()
+        total += labels.size(0)
+    train_loss = running_loss / total
+    train_acc = correct / total
+
+    # Validation
+    # Similar to above but no_grad
+    model.eval()
+    val_correct = 0
+    val_total = 0
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            preds = torch.argmax(outputs, dim=1)
+            val_correct += (preds == labels).sum().item()
+            val_total += labels.size(0)
+    val_acc = val_correct / val_total
+    print(f"Epoch {epoch+1}/{num_epochs} | Train Loss: {train_loss:.4f} | ")
+    print(f"Train Acc: {train_acc:.4f} | Val Acc: {val_acc:.4f}")
+    if val_acc > best_val_acc:
+        # Save if it has best so far accuracy
+        best_val_acc = val_acc
+        torch.save(model.state_dict(), "best_resnet18_bml.pth")
+print("Best validation accuracy:", best_val_acc)
+
+# Testing best model
+model.load_state_dict(torch.load("best_resnet18_bml.pth"))
+model.eval()
+all_preds = []
+all_labels = []
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        outputs = model(images)
+        preds = torch.argmax(outputs, dim=1)
+        
+        all_preds.extend(preds.cpu().numpy())
+        all_labels.extend(labels.numpy())
+print("Confusion Matrix:")
+print(confusion_matrix(all_labels, all_preds))
+print("\n")
+print("Classification Report:")
+print(classification_report(all_labels, all_preds, digits=4))
